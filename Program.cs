c2b082cf332806a3542f8d310ed1754a85446718
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2023-04-08 16:53:16 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] arr = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 1 + i; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        arr[0] = i;
+                        arr[1] = j;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
