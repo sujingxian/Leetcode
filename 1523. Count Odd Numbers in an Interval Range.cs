ae5bff6c855809d8bc448326fef764cce0f3d508
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2023-04-08 17:23:29 +1000

    Create 1523. Count Odd Numbers in an Interval Range.cs

---
+++ 1523. Count Odd Numbers in an Interval Range.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int CountOdds(int low, int high) {
+        int count = 0;
+        for (int i = low; i <= high; i ++)
+        {
+            if (i % 2 == 1)
+            {
+                count = count +1;
+            }
+
+        }
+        return count;
+        
+    }
+}
